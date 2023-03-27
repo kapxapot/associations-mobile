926f611aa41a0f1797bc250a48e60dac2daba082
Sergey Atroshchenko <kapxapot@gmail.com>
2023-03-27 11:57:20 +0400

    Adds NativeBase

--- App.tsx
+++ App.tsx
@@ -1,18 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Box, NativeBaseProvider } from 'native-base';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to Associations Mobile!</Text>
-    </View>
+    <NativeBaseProvider>
+      <Box>Welcome to Associations Mobile!</Box>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-});
